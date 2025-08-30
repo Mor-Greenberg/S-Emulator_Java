3620d4488814291f7faf4697e6dc86d6bab7b217
Mor-Greenberg <mor.green2001@gmail.com>
2025-08-30 12:36:54 +0300

    after debugging

--- console/src/MAIN.java
+++ console/src/MAIN.java
@@ -4,8 +4,6 @@ import menuHandling.PrintMenu;
 
 public class MAIN {
     public static void main(String[] args) {
-
-
         PrintMenu printMenu = new PrintMenu();
         printMenu.handleMenu();
     }
